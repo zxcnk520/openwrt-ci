e4dc3375895179244044bea8878d6d0a78690d4b
wei5i42 <wei5i42@qq.com>
2020-01-13 00:19:03 +0800

    update files: 20200113 00:19

--- diy.sh
+++ diy.sh
@@ -7,6 +7,9 @@ rm -rf /home/runner/openwrt/package/ctcgfw/openwrt-chinadns-ng/
 cp -R /home/runner/openwrt-package/lienol/luci-app-passwall/ /home/runner/openwrt/package/lienol/
 cp -R /home/runner/openwrt-package/package/chinadns-ng/ /home/runner/openwrt/package/lienol/
 
+echo 'Add luci-app-vssr'
+cp -R /home/runner/luci-app-vssr/ /home/runner/openwrt/package/
+
 echo 'Fix serverchan temperature bug...'
 sed -i "s?cut -c1-2 /sys/class/thermal/thermal_zone0/temp?cut -c1-2 /sys/class/hwmon/hwmon0/temp1_input?g" /home/runner/openwrt/package/ctcgfw/luci-app-serverchan/root/usr/bin/serverchan/serverchan
 
