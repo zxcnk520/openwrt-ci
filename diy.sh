2396fb47befcdd41ea257d8366b3d0f0519a58a1
wei5i42 <wei5i42@qq.com>
2020-02-21 23:47:00 +0800

    update files: 20200221 23:47

--- diy.sh
+++ diy.sh
@@ -21,7 +21,7 @@ rm -rf /home/runner/openwrt/package/ctcgfw/luci-app-vssr/
 cp -R /home/runner/luci-app-vssr/ /home/runner/openwrt/package/lean/
 
 echo 'Fix serverchan temperature bug'
-sed -i "s?cut -c1-2 /sys/class/thermal/thermal_zone0/temp?cut -c1-2 /sys/class/hwmon/hwmon0/temp1_input?g" /home/runner/openwrt/package/ctcgfw/luci-app-serverchan/root/usr/bin/serverchan/serverchan
+sed -i "s?/sys/class/thermal/thermal_zone\*/temp?/sys/class/hwmon/hwmon0/temp1_input?g" /home/runner/openwrt/package/ctcgfw/luci-app-serverchan/root/usr/bin/serverchan/serverchan
 
 echo 'Modify default theme'
 sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" /home/runner/openwrt/feeds/luci/collections/luci/Makefile
